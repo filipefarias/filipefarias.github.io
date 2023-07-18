ce86008e3dc7e6b6545735111cb7e0466600b0b9
filipefarias <filipe.fast@gmail.com>
2023-07-18 17:10:23 -0300

    Error Page Finished

--- filipefarias/src/components/Pages/ErrorPage.tsx
+++ filipefarias/src/components/Pages/ErrorPage.tsx
@@ -2,7 +2,7 @@ import Content from "../Content/content";
 
 function Error(): JSX.Element {
     return <Content title="Page Not Found">
-        <h2>Ops, essa páǵina não foi criada ainda.</h2>
+        <h2>Sorry, this page not exists...</h2>
     </Content>
 }
 
